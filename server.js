edd3a846fa0447609ff27b5fb36596923f8ebcd3
EdwinGnanaRajB <edwingnanarajb@gmail.com>
2026-05-27 21:48:34 +0530

    core updated

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: "https://task-manager-frontend-lime-one.vercel.app/",
+  credentials: true
 }));
 
 // Routes
