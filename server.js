efd094b6355bffa467f04df2e86eb47052158f43
EdwinGnanaRajB <edwingnanarajb@gmail.com>
2026-05-27 21:58:38 +0530

    Fixed CORS and duplicate express

--- server.js
+++ server.js
@@ -7,19 +7,9 @@ import cors from "cors";
 
 dotenv.config();
 
-console.log("JWT_SECRET:", process.env.JWT_SECRET);
-console.log("MONGO_URI:", process.env.MONGO_URI);
-
 const app = express();
 
 // Middleware
-require("dotenv").config();
-
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-
 app.use(cors({
   origin: "https://task-manager-frontend-lime-one.vercel.app",
   credentials: true
@@ -36,17 +26,15 @@ app.get("/", (req, res) => {
   res.send("Server Working");
 });
 
-// ✅ MongoDB connection (ONLY ONCE)
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
 
-    // Start server ONLY after DB connects
     const PORT = process.env.PORT || 5000;
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-
   })
   .catch((err) => console.log("Mongo Error:", err));
